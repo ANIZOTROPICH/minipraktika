5be47eb294050596bfb8308a405fb041214a6758
ANIZOTROPICH <60253711+ANIZOTROPICH@users.noreply.github.com>
2020-01-30 17:13:22 +0400

    Add files via upload

--- podschet.java
+++ podschet.java
@@ -6,17 +6,9 @@ public class podschet {
         public static void main(String[] args) {
  
             Scanner sc = new Scanner(System.in);
-            System.out.println("¬ведите строку");
-            String input = sc.nextLine();
-            int count = 0;
-            if(input.length() != 0){
-                count++;
-                for (int i = 0; i < input.length(); i++) {
-                    if(input.charAt(i) == ' '){
-                        count++;
-                    }
-                }
-            }
- 
-            System.out.println(count);
+            System.out.println("Vvedite stroku");
+            String stroka = sc.nextLine();
+            String[] Mas=stroka.split(" ");
+            System.out.println(Mas.length);
+            
         }}
